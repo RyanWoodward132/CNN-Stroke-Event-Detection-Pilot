73ef36188d57b3df461c64f29a7baf008b49664e
RyanWoodward132 <ryanjw132@gmail.com>
2026-06-29 15:55:48 +1000

    Updated RW.R and added Events.xlsx

--- Raw Processing/RW.R
+++ Raw Processing/RW.R
@@ -11,12 +11,12 @@ library(patchwork)
 library(stringr)
 
 #+ Import data
-acc = read.csv("Data/Raw/Temp/Accel/RW.csv", header = T)
-e05 = read.csv("Data/Raw/Temp/Events/RW_5.csv", header = F)
-e15 = read.csv("Data/Raw/Temp/Events/RW_15.csv", header = F)
-e25 = read.csv("Data/Raw/Temp/Events/RW_25.csv", header = F)
-e35 = read.csv("Data/Raw/Temp/Events/RW_35.csv", header = F)
-e45 = read.csv("Data/Raw/Temp/Events/RW_45.csv", header = F)
+acc = read.csv("Data/Temp/Accel/RW.csv", header = T)
+e05 = read.csv("Data/Temp/Events/RW_5.csv", header = F)
+e15 = read.csv("Data/Temp/Events/RW_15.csv", header = F)
+e25 = read.csv("Data/Temp/Events/RW_25.csv", header = F)
+e35 = read.csv("Data/Temp/Events/RW_35.csv", header = F)
+e45 = read.csv("Data/Temp/Events/RW_45.csv", header = F)
 
 #+ Convert acc$Time to elapsed time in seconds from first timestamp
 acc$Time = dmy_hms(acc$Time)
@@ -49,17 +49,27 @@ e45$V4 = as.numeric(e45$V4)
 
 #+ Adjust timecode of events to match and combine into one event df
 # Each section is unique and will need to be handled manually
-  # e05 - e15
-  # From the videos we can see that e05[6,] and e15[77,] are the same event
-  # take the time difference of these to sync them
-e15$V4 = e15$V4 + 38.42
-  # They are now synced, we can now remove duplicate events
+# Find matching events in each file using the videos and sync them
+# Then remove duplicate events from files
+
+# e05 - e15
+# e05[2,] and e15[1,] are the same event 
+e15$V4 = e15$V4 + 38.34
+# Removing duplicate files
+
+
+# e15 - e25
+# From the videos we can see that e15[27] and e25[1] are the same event
+e25$V4 = e25$V4 - 48.98
+
+# e25 - e35 
+# From the videos we can see that 
+
+
+# Remove duplicate events
+
 e05 = e05[-c(6:10),]
 e15 = e15[-c(1,2),]
-  # Repeat this process for all other event df
 
-  # e15 - e25
-  # From the videos we can see that e15[27] and e25[1] are the same event
-e25$V4 = e25$V4 - 48.98
 e15 = e15[-c(27:32), ]
 e25 = e25[-c(71:81), ]
